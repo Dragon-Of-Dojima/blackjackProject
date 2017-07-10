a6589787189a31dfc31e91719c3b14949b7daf99
root <root@AT1988.localdomain>
2017-07-10 17:51:01 -0400

    documentation additions

--- blackjackProject/CardRender2.java
+++ blackjackProject/CardRender2.java
@@ -25,27 +25,31 @@ import javax.imageio.ImageIO;
 import java.net.URL;
 import java.awt.image.*;
 
+//a class to render a physical representation of a card
 public class CardRender2 extends JComponent{
   
-  BufferedImage image;
-  String val;
-  String suit;
-  String filename;
+  BufferedImage image; //a image to be assigned to a CardRender2 instance
+  String val; //a name value
+  String suit; //a suit
+  String filename; //a file name, which is later fetched thru an instance method
   
+  //CardRender2 constructor, with a Card object as parameter
   public CardRender2(Card card) {
-    this.val = card.value.face;
-    this.suit = card.suit.toString();
-    filename = this.fetchCardFileLabel();
+    this.val = card.value.face;   //calls card's CardValue face
+    this.suit = card.suit.toString(); //calls toString on the enum suit
+    filename = this.fetchCardFileLabel(); //assigns the filename instance variable on the output of calling fetchCardFileLabel()
     
-    URL cardview = getClass().getResource("card deck/" + filename + ".png");
+    URL cardview = getClass().getResource("card deck/" + filename + ".png"); //used to pull the data at file location and convert into URL
     try {
       
-      image = ImageIO.read(cardview);
-    } catch (IOException e) {
+      image = ImageIO.read(cardview); //assigns the reading of cardview to the this.image 
+    } catch (IOException e) { //throw exception if not found and print stack trace
       e.printStackTrace();
     }
     
   }
+  
+  //works similarly to CardRender2(Card card) but used solely for dealer's first card
   public CardRender2(){
     this.val = null;
     this.suit = null;
@@ -60,6 +64,7 @@ public class CardRender2 extends JComponent{
     }
   }
   
+  //paint the card
   @Override
   protected void paintComponent(Graphics g) {
     Graphics2D g2 = (Graphics2D) g;
@@ -67,24 +72,26 @@ public class CardRender2 extends JComponent{
     g2.drawImage(image, 0, 0, null);
     
   }
-  
+  //get its size
   @Override
   public Dimension getPreferredSize() {
     return new Dimension(image.getWidth(), image.getHeight());
   }
   
+  // used to get the card's file label; all card file labels are their face's first character 
+  //(except 10, which gets first 2) and the first letter of their suit
   public String fetchCardFileLabel(){
     String first = null;
-    String second = suit.substring(0,1);
+    String second = suit.substring(0,1); //gets the first letter of the suit
     if(!val.equals("10"))
-      first = val.substring(0,1);
-    else
-      first = val.substring(0,2);
+      first = val.substring(0,1); //gets the first letter of file name for all numbers except 10
+    else                             
+      first = val.substring(0,2); //gets first characters of 10
     
-    return "" + first + second;
+    return "" + first + second; //returns the file name before extension
     
   }
-  
+  //tests to see if placeable on jframe and jpanel objects, as well as to test constructors
   public static void main(String[] args){
     CardRender2 dealerfirst = new CardRender2();
     JPanel j = new JPanel();
--- blackjackProject/Suit.java
+++ blackjackProject/Suit.java
@@ -1,3 +1,5 @@
- public enum Suit{
+//an enumeration of the four suits of cards
+
+public enum Suit{
     CLUBS, DIAMONDS, HEARTS, SPADES
   }
